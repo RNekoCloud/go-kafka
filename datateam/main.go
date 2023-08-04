8248dbe470574a78ccd512d006b804ca3cb1ac39
AvinFajarF <avinfajar6@gmail.com>
2023-08-04 21:48:35 +0700

    Feat: add consumer

---
+++ datateam/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+
+	topic := "FOO"
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "foo",
+		"auto.offset.reset": "smallest"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = consumer.Subscribe(topic, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		ev := consumer.Poll(100)
+		switch e := ev.(type) {
+		case *kafka.Message:
+			fmt.Printf("data team reading order: %s\n", e.Value)
+		case *kafka.Error:
+			fmt.Printf("%v\n", e)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
+
+type orderPlacer struct {
+	producer   *kafka.Producer
+	topic      string
+	deliverych chan kafka.Event
+}
+
+func NewOrderPlacer(p *kafka.Producer, topic string) *orderPlacer {
+	return &orderPlacer{
+		producer:   p,
+		topic:      topic,
+		deliverych: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *orderPlacer) placeOrder(orderType string, size int) error {
+
+	var (
+		format  = fmt.Sprintf("%s - %d", orderType, size)
+		payload = []byte(format)
+	)
+
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &op.topic, Partition: kafka.PartitionAny},
+		Value:          payload,
+	},
+		op.deliverych,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	<-op.deliverych
+	fmt.Printf("Place order on the queue %s\n", format)
+
+	return nil
+}
 
 func main() {
 
@@ -19,53 +56,12 @@ func main() {
 	if err != nil {
 		fmt.Printf("Failed to create producer: %s\n", err)
 	}
-	topic := "FOO"
 
-	go func() {
-
-		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-			"bootstrap.servers": "localhost:9092",
-			"group.id":          "foo",
-			"auto.offset.reset": "smallest"})
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = consumer.Subscribe(topic, nil)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for {
-			ev := consumer.Poll(100)
-			switch e := ev.(type) {
-			case *kafka.Message:
-				fmt.Printf("Consumed massage from the que: %s\n", e.Value)
-			case *kafka.Error:
-				fmt.Printf("%v\n", e)
-			}
-		}
-
-	}()
-
-	delivery_chan := make(chan kafka.Event, 10000)
-	for {
-
-		err = p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte("FOO"),
-		},
-			delivery_chan,
-		)
-
-		if err != nil {
+	op := NewOrderPlacer(p, "FOO")
+	for i := 0; i < 1000; i++ {
+		if err := op.placeOrder("Market: ", i+1); err != nil {
 			fmt.Println(err)
 		}
-
-		<-delivery_chan
-
 		time.Sleep(time.Second * 3)
 	}
 
